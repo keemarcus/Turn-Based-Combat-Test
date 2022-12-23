e7ab4dfb0787a5cef3343a2465ace76596b86101
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2022-12-23 14:07:24 -0500

    started file generation logic

--- Turn Based Combat Test/Assets/Scripts/TileMapGenerator.cs
+++ Turn Based Combat Test/Assets/Scripts/TileMapGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.IO;
+using UnityEditor;
 
 public class TileMapGenerator : MonoBehaviour
 {
@@ -26,6 +27,10 @@ public class TileMapGenerator : MonoBehaviour
         wallLayer = this.gameObject.transform.Find("Walls").GetComponent<Tilemap>();
         if (wallLayer == null) { Debug.Log("No tilemap found for wall layer"); }
 
+        // generate the ref file
+        //GenerateRefFile(20, 20,new[] {(2, 2, 5, 5), (10, 10, 15, 15) });
+        GenerateRefFile(50, 50, GenerateRoomPositions(50,50,5,20));
+
         // get the ref file
         string refData = refFile.ToString();
         string[] refDataLines = refData.Split('\n');
@@ -42,16 +47,17 @@ public class TileMapGenerator : MonoBehaviour
         numberOfColumns = 0;
         foreach(string line in refDataLines)
         {
-            if((line.Length - 1) > numberOfColumns) { numberOfColumns = line.Length - 1; }
+            if((line.Length) > numberOfColumns) { numberOfColumns = line.Length; }
         }
         Debug.Log("Tilemap Width = " + numberOfColumns);
 
+        ClearTileMap();
         GenerateTileMap(refDataLines);
     }
 
     private void GenerateTileMap(string[] refDataLines)
     {
-        for(int ctr = numberOfRows - 1; ctr > -1; ctr--)
+        for(int ctr = 0; ctr < numberOfRows; ctr++)
         {
             GenerateRow(ctr, refDataLines[ctr]);
         }
@@ -82,5 +88,107 @@ public class TileMapGenerator : MonoBehaviour
             }
             
         }
+    }
+
+    private void ClearTileMap()
+    {
+        groundLayer.ClearAllTiles();
+        wallLayer.ClearAllTiles();
+    }
+    private void GenerateRefFile(int width, int height, (int,int,int,int)[] rooms)
+    {
+        string map = "";
+        bool inRoom = false;
+        for(int ctr = 0; ctr < height; ctr++)
+        {
+            inRoom = false;
+            for(int ctr2 = 0; ctr2 < width; ctr2++)
+            {
+                foreach((int,int,int,int) room in rooms)
+                {
+                    if((ctr2 == room.Item1 || ctr2 == (room.Item3 + 1)) && (ctr >= room.Item2 & ctr <= room.Item4))
+                    {
+                        inRoom = !inRoom;
+                    }
+                }
+                
+                if(inRoom)
+                {
+                    map += 'g';
+                }
+                else
+                {
+                    map += 'w'; 
+                }
+            }
+            if(ctr < (height - 1)) { map += '\n'; }
+        }
+
+        Debug.Log(map);
+        File.WriteAllText(AssetDatabase.GetAssetPath(refFile), map);
+        EditorUtility.SetDirty(refFile);
+        AssetDatabase.SaveAssetIfDirty(refFile);
+        AssetDatabase.Refresh();
+    }
+
+    private (int, int, int, int)[] GenerateRoomPositions(int width, int height, int numberOfRooms, int maxFailures)
+    {
+        List<(int, int, int, int)> rooms = new List<(int, int, int, int)>();
+        int failures = 0;
+        int roomsGenerated = 0;
+
+        while (failures < maxFailures && roomsGenerated < numberOfRooms)
+        {
+            int startWidth = Random.Range((int) 2, width - 5);
+            int startHeight = Random.Range((int) 2, height - 5);
+            int endWidth = Random.Range(startWidth + 1, width - 1);
+            int endHeight = Random.Range(startHeight + 1, height - 1);
+            Debug.Log(startWidth + ", " + startHeight + ", " + endWidth + ", " + endHeight);
+            bool failure = false;
+
+            foreach((int, int, int, int) room in rooms)
+            {
+                if((startWidth >= room.Item1 && startWidth <= room.Item3) || (endWidth >= room.Item1 && endWidth <= room.Item3))
+                {
+                    if(((startHeight >= room.Item2 && startHeight <= room.Item4) || (endHeight >= room.Item2 && endHeight <= room.Item4)) || (startHeight <= room.Item2 && endHeight >= room.Item4))
+                    {
+                        failure = true;
+                        break;
+                    }
+                }
+
+                if ((startHeight >= room.Item2 && startHeight <= room.Item4) || (endHeight >= room.Item2 && endHeight <= room.Item4))
+                {
+                    if (((startWidth >= room.Item1 && startWidth <= room.Item3) || (endWidth >= room.Item1 && endWidth <= room.Item3)) || (startWidth <= room.Item1 && endWidth >= room.Item3))
+                    {
+                        failure = true;
+                        break;
+                    }
+                }
+
+                //if(((startWidth >= room.Item1 && startWidth <= room.Item3) || (endWidth >= room.Item1 && endWidth <= room.Item3) ) &&
+                //    (((startHeight >= room.Item2 && startHeight <= room.Item4) || (endHeight >= room.Item2 && endHeight <= room.Item4)) || 
+                //    (startHeight <= room.Item2 && endHeight >= room.Item4)))
+                //{
+                //    failure = true;
+                //    break;
+                //}
+            }
+
+            if(failure)
+            {
+                failures ++;
+                Debug.Log("FAIL");
+                continue;
+            }
+            else
+            {
+                rooms.Add((startWidth, startHeight, endWidth, endHeight));
+                roomsGenerated++;
+                Debug.Log("SUCCESS");
+            }
+        }
+
+        return rooms.ToArray();
     }
 }
